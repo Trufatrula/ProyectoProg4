2408d169d0ed2273ab14f72e7397488daff81bea
Asier Belloso <asierbelloso29@opendeusto.es>
2022-04-09 13:49:41 +0200

    Meter funciones de mas menus

--- src/menu.c
+++ src/menu.c
@@ -31,3 +31,56 @@ void clearIfNeeded(char *str, int max_line){
     if ((strlen(str) == max_line-1) && (str[max_line-2] != '\n'))
 		while (getchar() != '\n');
 }
+
+char mostrarMenuJugar()
+{
+    printf("-----------------------------------\n¿Que modo de juego quieres jugar? Escriba el numero correspondiente\n-----------------------------------\n");
+    printf("1. Reto Diario\n");
+    printf("2. WordleJALAD por numero de letras\n");
+    printf("3. WordleJALAD por tematica\n");
+    printf("4. Reto Contrareloj\n");
+    printf("5. WordleJALAD : Multijugador\n");
+    printf("6. Volver\n");
+    printf("-> \n");
+    fflush(stdout);
+    char opcion[MAX];
+    fgets(opcion, MAX, stdin);
+
+    return *opcion;
+
+}
+
+char mostrarMenuEstadisticas()
+{
+    printf("-----------------------------------\n¿Que estadisticas quieres consultar? Escriba el numero correspondiente\n-----------------------------------\n");
+    printf("1. Horas jugadas\n");
+    printf("2. Puntuacion total : Reto Diario\n");
+    printf("3. Puntuacion total : WordleJALAD por numero de letras\n");
+    printf("4. Puntuacion total : WordleJALAD por tematica\n");
+    printf("5. Puntuacion total : Reto Contrareloj\n");
+    printf("6. Puntuacion total : WordleJALAD : Multijugador\n");
+    printf("Pulsa 'q' para volver\n");
+    printf("-> \n");
+    fflush(stdout);
+    char opcion[MAX];
+    fgets(opcion, MAX, stdin);
+
+    return *opcion;
+
+}
+
+char mostrarMenuCuenta(char* usuario)
+{
+    printf("-CUENTA DE %s -\n",usuario);
+    printf("1. Cambiar nombre usuario\n");
+    printf("2. Cambiar contraseña del usuario\n");
+    printf("3. Borrar cuenta\n");
+    printf("Pulsa 'q' para volver\n");
+    printf("-> \n");
+    fflush(stdout);
+    char opcion[MAX];
+    fgets(opcion, MAX, stdin);
+
+    return *opcion;
+
+}
--- src/menu.h
+++ src/menu.h
@@ -3,6 +3,9 @@
 
 char mostrarMenu();
 char mostarMenuUsuario();
+char mostrarMenuJugar();
+char mostrarMenuEstadisticas();
+char mostrarMenuCuenta(char* usuario);
 void registro();
 void inicioSesion();
 void clearIfNeeded(char *str, int max_line);
