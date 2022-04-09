ec7e22326c4b1dc437ef3497925b895c8bbad852
DavidBarrenechea <david.barrenechea@opendeusto.es>
2022-04-09 13:08:04 +0200

    volver al original
    prueba

--- src/main.c
+++ src/main.c
@@ -2,13 +2,7 @@
 
 int main() {
     printf("Hello world\n");
-<<<<<<< HEAD
 
-
-    printf("Hola bbsita");
-=======
-    printf("cacahuete");
->>>>>>> 7e658f2120a56f08d4b4493f7af85dddc383619d
     return 0;
 
 }
