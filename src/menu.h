ac7f74bd6f2e701ee6de089a53cbf13e07cff059
JorgeAlonsoSuS <jorge.alonso2002@opendeusto.es>
2022-04-12 11:40:01 +0200

    Ñ

--- src/menu.h
+++ src/menu.h
@@ -5,7 +5,7 @@ int opcion(char* mensaje, int cantidad, char** opciones);
 void menuSesion();
 void menuPrincipal();
 void menuEdicionUsuariosAdmin();
-void menuAñadirUsuarios();
+void menuAnyadirUsuarios();
 //char mostrarMenuEstadisticas();
 //char mostrarMenuCuenta(char* usuario);
 void clearIfNeeded(char *str, int max_line);
