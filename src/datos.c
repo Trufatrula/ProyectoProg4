271e3786bf15cada23dac93d42efd9ba63d6c7a2
Lander Gallastegi <landergallastegi@gmail.com>
2022-04-12 11:52:54 +0200

    Fix

--- src/datos.c
+++ src/datos.c
@@ -128,7 +128,7 @@ int iniciarSesion(char* nick, char* contrasena, char* token) {
 		    printf("%s\n", sqlite3_errmsg(__baseDeDatosActual));
 		    return result;
 	    }
-        return result;
+        return SQLITE_OK;
     }
 	return SQLITE_ERROR;
 }
@@ -181,8 +181,7 @@ int generarTablas() {
 	User_Nick TEXT NOT NULL,\
 	Normal_Score INTEGER NOT NULL,\
 	League_Points INTEGER NOT NULL,\
-	FOREIGN KEY(User_Nick) REFERENCES Usuario(Nick) ON DELETE CASCADE,\
-	PRIMARY KEY(User_Nick))";
+	PRIMARY KEY (User_Nick)) REFERENCES Usuario(Nick) ON DELETE CASCADE";
 
     result = sqlite3_prepare_v2(__baseDeDatosActual, sqlPuntuacion, -1, &stmt, NULL) ;  //No estoy seguro del parámetro 3 de la función, si debe ser strlen(sql), strlen(sql)+1, strlen(sql)-1
 
@@ -195,7 +194,7 @@ int generarTablas() {
 	Palabra	TEXT NOT NULL,\
 	Tema TEXT NOT NULL,\
 	Idioma	TEXT NOT NULL,\
-	PRIMARY KEY(Palabra))";
+	PRIMARY KEY (Palabra))";
 
     result = sqlite3_prepare_v2(__baseDeDatosActual, sqlDiccionario, -1, &stmt, NULL) ;
 
@@ -222,7 +221,8 @@ int generarTablas() {
 	User_Nick	TEXT NOT NULL,\
 	Token TEXT NOT NULL,\
     Expira INTEGER NOT NULL,\
-	PRIMARY KEY(Token))";
+	PRIMARY KEY(Token)),\
+	FOREGIN KEY(User_Nick) REFERENCES Usuario(Nick) ON DELETE CASCADE";
 
     result = sqlite3_prepare_v2(__baseDeDatosActual, sqlToken, -1, &stmt, NULL) ;
     if (result != SQLITE_OK) {
