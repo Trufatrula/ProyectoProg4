1b3a6db5cbea9c017762182bdf32b8bb5c5c2216
Trufatrula <andonitorrescauso@gmail.com>
2022-04-13 18:26:55 +0200

    sesion.c y sesion.h

---
+++ src/sesion.c
@@ -0,0 +1,34 @@
+#include "sesion.h"
+#include <string.h>
+#include <stdio.h>
+
+char __tokenactual[33];
+
+void establecerToken(char* token){
+    if(strlen(token) == 33){
+        strcpy(__tokenactual, token);
+    }
+}
+
+void getToken(char* token){
+    strcpy(token, __tokenactual);
+}
+
+void guardarToken(char* nomFichero){
+    FILE * fichero;
+    fichero = fopen(nomFichero, "r");
+    fputs(__tokenactual, fichero);
+    fclose(fichero);
+}
+
+int cargarToken(char*nomFichero){
+    FILE * fichero;
+    fichero = fopen(nomFichero, "r");
+    if(fichero == NULL){
+        return 1;
+    }
+    fread(__tokenactual, 32, 1, fichero);
+    __tokenactual[32] = 0;
+    fclose(fichero);
+    return 0;
+}
---
+++ src/sesion.h
@@ -0,0 +1,9 @@
+#ifndef SESION
+#define SESION
+
+void establecerToken(char* token);
+void getToken(char* token);
+void guardarToken(char* fichero);
+int cargarToken(char* fichero);
+
+#endif
