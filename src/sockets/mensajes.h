e605c502cd21e803a7c877db11f848084665f0ee
Lander Gallastegi <landergallastegi@gmail.com>
2022-05-29 14:43:05 +0200

    Mensajes

---
+++ src/sockets/mensajes.h
@@ -0,0 +1,13 @@
+#ifndef SOCKETMENSAJES
+#define SOCKETMENSAJES
+
+#define NONE       0
+#define LOGIN      1
+#define REGISTER   2
+#define TOKENLOGIN 3
+#define PARTIDA    4
+#define PROBAR     5
+#define PROBARRESP 6
+#define ERROR      128
+
+#endif
