9275e27c32e446b6db8c2fbb5d6b9f04b9038f54
Lander Gallastegi <landergallastegi@gmail.com>
2022-05-30 17:13:17 +0200

    fIX MENSAJES

--- src/sockets/mensajes.h
+++ src/sockets/mensajes.h
@@ -1,13 +1,14 @@
 #ifndef SOCKETMENSAJES
 #define SOCKETMENSAJES
 
-#define NONE       0
-#define LOGIN      1
-#define REGISTER   2
-#define TOKENLOGIN 3
-#define PARTIDA    4
-#define PROBAR     5
-#define PROBARRESP 6
-#define ERROR      128
+#define NONE         0
+#define LOGIN        1
+#define REGISTER     2
+#define TOKENLOGIN   3
+#define PARTIDA      4
+#define PROBAR       5
+#define PROBARRESP   6
+#define CLIENTESALIR 64
+#define JALADERROR   128
 
 #endif
